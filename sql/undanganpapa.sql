032c55d0fb4a92295f9ce44a56db7dd55ecc6f85
Edo C. Setiawan <edocsetiawan@gmail.com>
2024-09-30 09:33:33 +0700

    update sql

---
+++ sql/undanganpapa.sql
@@ -0,0 +1,11 @@
+INSERT INTO edomonike (alias,name) 
+	VALUES
+('yl','Bapak Yongki & Ibu Loanita'),
+('kti','Bapak Kwa Tjwan Ien & istri'),
+('iis','Ibu ⁠Ida, Ine, Sha-sha'),
+('se','Bapak Samoel & Ibu Elly'),
+('dlf','Bapak Donny'),
+('it','Bapak Ing Tjiang & Mak Yok'),
+('eiv','Ibu Elisabeth (Iiet'),
+('dk','Bapak Dwi Kristianto'),
+('ktl','Koh Tiauw, Koh Liong');
